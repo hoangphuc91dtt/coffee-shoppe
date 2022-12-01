25b1ef0c05b3e8dce4236136da3417ea3d3b98af
hoangphuc91 <hoangphuc91dtt@gmail.com>
2022-12-01 15:56:51 +0700

    update:01122022

--- js/homepage.js
+++ js/homepage.js
@@ -1,8 +1,11 @@
+const authors = document.querySelector(".testimonial__author-list");
 const cards = document.querySelector(".popular__card-list");
+const cardsMenu = document.querySelector(".popular__card-list2");
 const btnshop = document.querySelector(".navbar__shop");
 const btngiohang = document.querySelector(".navbar__giohang");
 const addgiohang = document.querySelector("#giohang");
 const hiddenElement = document.querySelectorAll(".hidden");
+const navbarHidden = document.querySelectorAll(".navbar__link");
 
 const arrGioHang = [];
 function renderCardInCart(obj) {
@@ -25,20 +28,20 @@ function renderCardInCart(obj) {
         .join("");
 }
 
-function renderHomepage() {
+function renderCoffee() {
     fetch("https://6369cb9028cd16bba72488d3.mockapi.io/coffee")
         .then((res) => res.json())
         .then((data) => {
             cards.innerHTML = data
                 .map(
-                    (obj) => ` <div class="popular__card " card-id="${obj.id}" >
+                    (obj) => ` <div class="popular__card" card-id="${obj.id}" >
         <div class="popular__rating">
             <img
                 src="./img/rating_product.png"
                 alt=""
             />
         </div>
-        <div class="popular__card--img ">
+        <div class="popular__card--img">
             <img
                 src=${obj.img}
                 alt=""
@@ -65,7 +68,69 @@ function renderHomepage() {
                 .slice(0, 3)
                 .join("");
 
+            //  cards.innerHTML = data
+            cardsMenu.innerHTML = data
+                .map(
+                    (obj) => ` <div class="popular__card " card-id="${obj.id}" >
+                    <div class="popular__rating">
+                        <img
+                            src="./img/rating_product.png"
+                            alt=""
+                        />
+                    </div>
+                    <div class="popular__card--img">
+                        <img
+                            src=${obj.img}
+                            alt=""
+                        />
+                    </div>
+                    <div class="popular__card--info">
+                        <div class="popular__card--name">
+                            ${obj.name}
+                        </div>
+                        <div class="popular__card--cost">${obj.cost} K</div>
+                    </div>
+                    <div class="popular__card--decribe">
+                    <div class="popular__card--subline">
+                        ${obj.subline}
+                    </div>
+                        <div class="popular__card--buy">
+                            <img
+                                src="./img/card.png"
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                    </div>`
+                )
+                .slice(4, 13)
+                .join("");
             muahang();
+        });
+}
+function renderUser() {
+    fetch("https://6369cb9028cd16bba72488d3.mockapi.io/user")
+        .then((res) => res.json())
+        .then((data) => {
+            authors.innerHTML = data
+                .map(
+                    (obj) => `<div class="testimonial__author">
+                    <div class="testimonial__author--avt">
+                        <img src="${obj.avt}" alt="" />
+                    </div>
+                    <div class="testimonial__author--info">
+                        <div class="testimonial__author--name">
+                            ${obj.name}
+                        </div>
+                        <div class="testimonial__author--title">
+                            I really love the cappucino, the coffee
+                            was very smooth
+                        </div>
+                    </div>
+                </div>`
+                )
+                .slice(0, 3)
+                .join("");
         });
 }
 
@@ -93,6 +158,7 @@ function muahang() {
         });
     });
 }
+
 function cleangiohang() {
     const clean = document.querySelectorAll(".clean");
     const sanpham = document.querySelectorAll(".navbar__giohang--sanpham");
@@ -120,8 +186,25 @@ function hiddenElm() {
     hiddenElement.forEach((el) => observer.observe(el));
 }
 
+function navbarActive() {
+    navbarHidden.forEach((button) => {
+        button.addEventListener("click", () => {
+            removeClass();
+            button.classList.add("active");
+        });
+    });
+}
+
+function removeClass() {
+    navbarHidden.forEach((button) => {
+        button.classList.remove("active");
+    });
+}
+
 function homepage() {
-    renderHomepage();
+    navbarActive();
+    renderCoffee();
+    renderUser();
     giohang();
     muahang();
     hiddenElm();
