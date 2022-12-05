0a86f00e834ddfe0bb286f80a552cdd3214ede4d
hoangphuc91 <hoangphuc91dtt@gmail.com>
2022-12-06 01:40:59 +0700

    06122022

--- js/homepage.js
+++ js/homepage.js
@@ -1,78 +1,13 @@
 const authors = document.querySelector(".testimonial__author-list");
 const cards = document.querySelector(".popular__card-list");
 const cardsMenu = document.querySelector(".popular__card-list2");
-const btnshop = document.querySelector(".navbar__shop");
 const btngiohang = document.querySelector(".navbar__giohang");
 const addgiohang = document.querySelector("#giohang");
 const hiddenElement = document.querySelectorAll(".hidden");
 const navbarHidden = document.querySelectorAll(".navbar__link");
+const btnshop = document.querySelector(".navbar__shop");
+const btnYourCoffee = document.querySelector(".about__content--btn-get");
 const showMore_btn = document.querySelector(".popular__btn-showmore");
-
-const arrGioHang = [];
-function renderCardInCart(obj) {
-    arrGioHang.push(obj);
-    addgiohang.innerHTML = arrGioHang
-        .map(
-            (obj) => `
-    <div class="navbar__giohang--sanpham">
-        <div class="navbar__giohang--image">
-            <img src=${obj.img} alt="" />
-        </div>
-        <div class="navbar__giohang--name">${obj.name}</div>
-        <div class="navbar__giohang--cost">${obj.cost} K</div>
-        <div class="navbar__giohang--x">              
-            <img src="./img/rubbish-bin.png" alt="" />
-         </div>
-    </div>
-    `
-        )
-        .join("");
-    removeItemGioHang();
-}
-
-function renderCoffee() {
-    fetch("https://6369cb9028cd16bba72488d3.mockapi.io/coffee")
-        .then((res) => res.json())
-        .then((data) => {
-            cardsMenu.innerHTML = data
-                .map(
-                    (obj) => ` <div class="popular__card " card-id="${obj.id}" >
-                    <div class="popular__rating">
-                        <img
-                            src="./img/rating_product.png"
-                            alt=""
-                        />
-                    </div>
-                    <div class="popular__card--img">
-                        <img
-                            src=${obj.img}
-                            alt=""
-                        />
-                    </div>
-                    <div class="popular__card--info">
-                        <div class="popular__card--name">
-                            ${obj.name}
-                        </div>
-                        <div class="popular__card--cost">${obj.cost} K</div>
-                    </div>
-                    <div class="popular__card--decribe">
-                    <div class="popular__card--subline">
-                        ${obj.feedback}
-                    </div>
-                        <div class="popular__card--buy">
-                            <img
-                                src="./img/card.png"
-                                alt=""
-                            />
-                        </div>
-                    </div>
-                    </div>`
-                )
-                .join("");
-
-            muahang();
-        });
-}
 
 function renderCoffeebest() {
     fetch("https://6369cb9028cd16bba72488d3.mockapi.io/coffee-food")
@@ -118,6 +53,53 @@ function renderCoffeebest() {
             muahangBestCoffee();
         });
 }
+
+// vu thien
+function renderCoffee() {
+    fetch("https://6369cb9028cd16bba72488d3.mockapi.io/coffee")
+        .then((res) => res.json())
+        .then((data) => {
+            cardsMenu.innerHTML = data
+                .map(
+                    (obj) => ` <div class="popular__card " card-id="${obj.id}" >
+                    <div class="popular__rating">
+                        <img
+                            src="./img/rating_product.png"
+                            alt=""
+                        />
+                    </div>
+                    <div class="popular__card--img">
+                        <img
+                            src=${obj.img}
+                            alt=""
+                        />
+                    </div>
+                    <div class="popular__card--info">
+                        <div class="popular__card--name">
+                            ${obj.name}
+                        </div>
+                        <div class="popular__card--cost">${obj.cost} K</div>
+                    </div>
+                    <div class="popular__card--decribe">
+                    <div class="popular__card--subline">
+                        ${obj.feedback}
+                    </div>
+                        <div class="popular__card--buy">
+                            <img
+                                src="./img/card.png"
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                    </div>`
+                )
+                .join("");
+
+            muahang();
+        });
+}
+
+// bich ngoc
 function renderUser() {
     fetch("https://6369cb9028cd16bba72488d3.mockapi.io/user")
         .then((res) => res.json())
@@ -144,33 +126,6 @@ function renderUser() {
         });
 }
 
-function getCoffeeId(id) {
-    fetch(`https://6369cb9028cd16bba72488d3.mockapi.io/coffee/${id}`)
-        .then((res) => res.json())
-        .then((data) => {
-            renderCardInCart(data);
-        });
-}
-function getBestCoffeeId(id) {
-    fetch(`https://6369cb9028cd16bba72488d3.mockapi.io/coffee-food/${id}`)
-        .then((res) => res.json())
-        .then((data) => {
-            renderCardInCart(data);
-        });
-}
-
-function giohang() {
-    const btnYourCoffee = document.querySelector(".about__content--btn-get");
-    btnshop.addEventListener("click", (e) => {
-        e.preventDefault();
-        btngiohang.classList.toggle("disable");
-    });
-    btnYourCoffee.addEventListener("click", (e) => {
-        e.preventDefault();
-        btngiohang.classList.toggle("disable");
-    });
-}
-
 function muahang() {
     const btncard = document.querySelectorAll(".popular__card");
     btncard.forEach((btn) => {
@@ -190,10 +145,58 @@ function muahangBestCoffee() {
         });
     });
 }
+// lấy id
+function getCoffeeId(id) {
+    fetch(`https://6369cb9028cd16bba72488d3.mockapi.io/coffee/${id}`)
+        .then((res) => res.json())
+        .then((data) => {
+            addCardInShop(data);
+        });
+}
+function getBestCoffeeId(id) {
+    fetch(`https://6369cb9028cd16bba72488d3.mockapi.io/coffee-food/${id}`)
+        .then((res) => res.json())
+        .then((data) => {
+            addCardInShop(data);
+        });
+}
+//  add card in shop
+const arrGioHang = [];
+function addCardInShop(obj) {
+    arrGioHang.push(obj);
+    addgiohang.innerHTML = arrGioHang
+        .map(
+            (obj) => `
+    <div class="navbar__giohang--sanpham">
+        <div class="navbar__giohang--image">
+            <img src=${obj.img} alt="" />
+        </div>
+        <div class="navbar__giohang--name">${obj.name}</div>
+        <div class="navbar__giohang--cost">${obj.cost} K</div>
+        <div class="navbar__giohang--x">              
+            <img src="./img/rubbish-bin.png" alt="" />
+         </div>
+    </div>
+    `
+        )
+        .join("");
+    removeItemGioHang();
+}
+// di chuyển đến menu
+function giohang() {
+    btnshop.addEventListener("click", (e) => {
+        e.preventDefault();
+        btngiohang.classList.toggle("disable");
+    });
+    btnYourCoffee.addEventListener("click", (e) => {
+        e.preventDefault();
+        btngiohang.classList.toggle("disable");
+    });
+}
+// remove ItemGioHang
 
 function removeItemGioHang() {
     const timesItems = document.querySelectorAll(".navbar__giohang--x");
-
     timesItems.forEach(
         (timesItem, index) =>
             (timesItem.onclick = () => {
@@ -202,6 +205,8 @@ function removeItemGioHang() {
             })
     );
 }
+
+//  scroll secction
 function hiddenElm() {
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
@@ -215,6 +220,7 @@ function hiddenElm() {
     hiddenElement.forEach((el) => observer.observe(el));
 }
 
+// active item navbar
 function navbarActive() {
     navbarHidden.forEach((button) => {
         button.addEventListener("click", () => {
@@ -223,7 +229,7 @@ function navbarActive() {
         });
     });
 }
-
+// active item navbar
 function removeClass() {
     navbarHidden.forEach((button) => {
         button.classList.remove("active");
